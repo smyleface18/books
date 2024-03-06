b2a58cfc8a805163674edbeb09298658a266bf72
ca30850@gmail.com <ca30850@gmai.com>
2024-03-06 15:55:49 -0500

    trabajo en clases prestar libro

--- src/main/java/com/mycompany/book/Book.java
+++ src/main/java/com/mycompany/book/Book.java
@@ -3,6 +3,7 @@
  */
 
 package com.mycompany.book;
+import java.util.Scanner;
 
 /**
  *
@@ -10,7 +11,53 @@ package com.mycompany.book;
  */
 public class Book {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    String title;
+    String author;
+    String genre;
+    String datePub;
+    String avalible; // El estado del libro
+    String quantity;
+    String loanTime;
+    String user;
+    String loanDate; // la fecha que salio
+    String returnDate;  
+
+    public Book(String title, String author, String genre, String datePub, String avalible, String quantity, String loanTime, String user, String loanDate, String returnDate) {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.datePub = datePub;
+        this.avalible = avalible;
+        this.quantity = quantity;
+        this.loanTime = loanTime;
+        this.user = user;
+        this.loanDate = loanDate;
+        this.returnDate = returnDate;
     }
+    
+    public void lendBook(){
+        if(this.avalible.equalsIgnoreCase("DISPONIBLE")){
+                this.avalible = "NO DISPONIBLE";
+                System.out.println("Ingresa el nombre de usuario");
+                Scanner read = new Scanner (System.in);
+                this.user = read.nextLine();
+        }
+        
+        else{
+            System.out.println("El libro no esta disponible");
+        }
+        
+    }
+    
+    public void returnBook(){
+         this.avalible = "DISPOBIBLE";
+         this.user = "";
+    }
+    
+    public void printBook(){
+        System.out.println(this.title+" | "+this.author+" | "+this.genre+" | "+this.datePub + " | "+this.avalible+" | "+this.quantity+" | "+this.loanTime+" | "+this.user+" | "+this.loanDate+" | "+this.returnDate);
+    }
+    
+    
+    
 }
---
+++ src/main/java/mainBook/mainBook.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mainBook;
+import com.mycompany.book.Book;
+/**
+ *
+ * @author SCIS2PC08
+ */
+public class mainBook {
+    
+    public static void main(String[] args) {
+        
+        Book book_one = new Book("El viaje al centro de la tierra","Julio verne","Fantasia","1864","NO DISPONIBLE","1","3","","","");
+        Book book_two = new Book("La iliade","Homero","Epico","VII","DISPONIBLE","1","3","","","");
+        Book book_three = new Book("El principito"," Saint-Exupéry.","Literatura infantil","1943","DISPONIBLE","1","3","","",""); 
+        Book book_four = new Book("El conflicto de los siglos","Ellen G. White","Literatura cristiana","	1913","DISPONIBLE","1","3","","","");
+        Book book_five = new Book("La Odisea","HOMERO","Epico","VII","DISPONIBLE","1","3","","","");
+        
+       
+        
+        
+    }
+    
+}
